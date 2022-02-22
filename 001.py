60af7a7f5e57090a103f0e55f824817d99d5011c
_illaech <illaech@gmail.com>
2022-02-22 16:59:49 +0300

    docs(001): comments

--- 001.py
+++ 001.py
@@ -8,29 +8,27 @@ You can return the answer in any order.
 
 
 Example 1:
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 Example 2:
+    Input: nums = [3,2,4], target = 6
+    Output: [1,2]
 
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
 Example 3:
-
-Input: nums = [3,3], target = 6
-Output: [0,1]
+    Input: nums = [3,3], target = 6
+    Output: [0,1]
 
 
 Constraints:
+    2 <= nums.length <= 10⁴
+    -10⁹ <= nums[i] <= 10⁹
+    -10⁹ <= target <= 10⁹
+    Only one valid answer exists.
 
-2 <= nums.length <= 104
--109 <= nums[i] <= 109
--109 <= target <= 109
-Only one valid answer exists.
 
-
-Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+Follow-up: Can you come up with an algorithm that is less than O(n²) time complexity?
 """
 
 
@@ -38,9 +36,17 @@ def solve(nums: list[int], target) -> list[int]:
     diffs = {target - x for x in nums}
     numbers = set(nums).intersection(diffs)
 
+    # Due to the last constrain, `numbers` can only be 1 to 3 items long:
+    #   1 — if result is `target / 2`,
+    #   2 — two different numbers,
+    #   3 — two different numbers and `target / 2`.
+    # Thus, cases 1 and 3 must be slightly corrected to get the expected result.
+
     if len(numbers) == 1:
+        # duplicate number
         return list(numbers) * 2
     if len(numbers) > 2:
+        # exclude `target / 2`
         return list(numbers - {target / 2})
     return list(numbers)
 
@@ -49,13 +55,14 @@ class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         numbers = solve(nums, target)
 
-        indicies = [0] * 2
+        indices = [0] * 2  # makes a list only two items long it terms of used memory
         for i, n in enumerate(numbers):
             idx = nums.index(n)
-            nums[idx] = None
-            indicies[i] = idx
+            nums[idx] = None  # exclude used index from original array (for `target / 2` situation)
+            indices[i] = idx
 
-        return indicies
+        # Because I don't shuffle or sort any lists, indices are in ascending order, so just return them as-is.
+        return indices
 
 
 def main():
