d17c4024d3de35a7faa4fd3b679f81fb0f951e0d
_illaech <illaech@gmail.com>
2022-02-22 18:00:30 +0300

    docs(001): add results

--- 001.py
+++ 001.py
@@ -1,34 +1,37 @@
 """1. Two Sum
 
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+Description:
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they
+    add up to target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-You can return the answer in any order.
-
-
-Example 1:
-    Input: nums = [2,7,11,15], target = 9
-    Output: [0,1]
-    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-
-Example 2:
-    Input: nums = [3,2,4], target = 6
-    Output: [1,2]
-
-Example 3:
-    Input: nums = [3,3], target = 6
-    Output: [0,1]
+    You can return the answer in any order.
 
 
-Constraints:
-    2 <= nums.length <= 10⁴
-    -10⁹ <= nums[i] <= 10⁹
-    -10⁹ <= target <= 10⁹
-    Only one valid answer exists.
+    Example 1:
+        Input: nums = [2,7,11,15], target = 9
+        Output: [0,1]
+        Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+    Example 2:
+        Input: nums = [3,2,4], target = 6
+        Output: [1,2]
+
+    Example 3:
+        Input: nums = [3,3], target = 6
+        Output: [0,1]
 
 
-Follow-up: Can you come up with an algorithm that is less than O(n²) time complexity?
+    Constraints:
+        2 <= nums.length <= 10⁴
+        -10⁹ <= nums[i] <= 10⁹
+        -10⁹ <= target <= 10⁹
+        Only one valid answer exists.
+
+Results:
+    Runtime: 56 ms, faster than 96.30% of Python3 online submissions for Two Sum.
+    Memory Usage: 16 MB, less than 6.61% of Python3 online submissions for Two Sum.
 """
 
 
@@ -55,13 +58,14 @@ class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         numbers = solve(nums, target)
 
-        indices = [0] * 2  # makes a list only two items long it terms of used memory
+        indices = [0] * 2  # makes a list only two items long in terms of used memory
         for i, n in enumerate(numbers):
             idx = nums.index(n)
             nums[idx] = None  # exclude used index from original array (for `target / 2` situation)
             indices[i] = idx
 
         # Because I don't shuffle or sort any lists, indices are in ascending order, so just return them as-is.
+        # But, anyway, description says that order doesn't matter.
         return indices
 
 
